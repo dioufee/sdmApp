cb6d88b508bab29e37a77863c3979be8183b064f
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-09-28 01:35:08 +0000

    Update ensae.R

--- ensae.R
+++ ensae.R
@@ -117,12 +117,65 @@ sdmApp_RasterPlot<-function(rasterLayer){
   colnames(map_df) <- c("Easting", "Northing", "MAP")
   basePlot1 <- ggplot2::ggplot() + ggplot2::geom_raster(data = map_df, 
                                                         ggplot2::aes_string(y = "Northing", x = "Easting", fill = "MAP"))
-  basePlot1<-basePlot1 + ggplot2::theme_bw() + ggplot2::labs(x = "Longitude", y = "Latitude") +
-    ggtitle(label = names(rasterLayer)) + 
-    theme(plot.title = element_text(hjust = 0.5, size = 10)) + 
+  # basePlot1<-basePlot1 + ggplot2::theme_bw() + ggplot2::labs(x = "Longitude", y = "Latitude") +
+  #   ggtitle(label = names(rasterLayer)) + 
+  basePlot1<-basePlot1 + theme(plot.title = element_text(hjust = 0.5, size = 10)) + 
     ggplot2::scale_fill_gradientn(name = " ", colours = rev(terrain.colors(10)))
   return(basePlot1)
 }
+
+string_code <- reactive({
+  p <- paste("sdmApp_RasterPlot(map) + ")
+  if (input$label_axes) 
+    p <- paste(p, "+ labs(x = 'input$lab_x', y = 'input$lab_y')")
+  if (input$add_title) 
+    p <- paste(p, "+ ggtitle('input$title')")
+  # if (input$adj_leg == "Change legend") 
+  #   p <- paste(p, "+ labs(", if (gg_fil) 
+  #     "fill"
+  #     else "colour", " = 'input$leg_ttl')", 
+  #     sep = "")
+  # if (input$adj_col) 
+  #   p <- paste(p, "+ scale_", if (gg_fil) 
+  #     "fill"
+  #     else "colour", "_brewer(palette = 'input$palet')", 
+  #     sep = "")
+  p <- paste(p, "+", input$theme)
+  if (input$adj_fnt_sz || input$adj_fnt || input$rot_txt || 
+      input$adj_leg != "Keep legend as it is" || 
+      input$adj_grd) {
+    p <- paste(p, paste(" + theme(\n    ", 
+                        if (input$adj_fnt_sz) 
+                          "axis.title = element_text(size = input$fnt_sz_ttl),\n    ", 
+                        if (input$adj_fnt_sz) 
+                          "axis.text = element_text(size = input$fnt_sz_ax),\n    ", 
+                        if (input$adj_fnt) 
+                          "text = element_text(family = 'input$font'),\n    ", 
+                        if (input$rot_txt) 
+                          "axis.text.x = element_text(angle = 45, hjust = 1),\n    ", 
+                        if (input$adj_leg == "Remove legend") 
+                          "legend.position = 'none',\n    ", 
+                        if (input$adj_leg == "Change legend") 
+                          "legend.position = 'input$pos_leg',\n    ", 
+                        if (input$grd_maj) 
+                          "panel.grid.major = element_blank(),\n    ", 
+                        if (input$grd_min) 
+                          "panel.grid.minor = element_blank(),\n    ", 
+                        ")", sep = ""), sep = "")
+  }
+  p <- str_replace_all(p, c(`input\\$lab_x` = as.character(input$lab_x), 
+                            `input\\$lab_y` = as.character(input$lab_y), 
+                            `input\\$title` = as.character(input$title), 
+                            `input\\$palet` = as.character(input$palet), 
+                            `input\\$fnt_sz_ttl` = as.character(input$fnt_sz_ttl), 
+                            `input\\$fnt_sz_ax` = as.character(input$fnt_sz_ax), 
+                            `input\\$font` = as.character(input$font), 
+                            `input\\$leg_ttl` = as.character(input$leg_ttl), 
+                            `input\\$pos_leg` = as.character(input$pos_leg))
+  )
+  p <- str_replace_all(p, ",\n    \\)", "\n  \\)")
+  p
+})
 
 PASpecies<-function(rasterLayer){
   samp <- raster::sampleRegular(rasterLayer, 5e+05, asRaster = TRUE)
@@ -423,13 +476,7 @@ cirad_hema <-function()
             } else {
               map = data$Env[[i]]
             }
-            
-            # a=try(spplot(map,
-            #        main = names(data$Env)[i],
-            #        xlab = 'Longitude (\u02DA)',
-            #        ylab = 'Latitude (\u02DA)',
-            #        col.regions = rev(terrain.colors(10000))))
-            a =try(sdmApp_RasterPlot(map))
+            a =try(eval(parse(text = string_code())))
             if(inherits(a, 'try-error')){
               output$Envbugplot <- renderUI(p('Can not plot this raster! Please verify it and try again.'))
             }
@@ -622,7 +669,7 @@ observeEvent(input$file_type,{
       )
       txt_rasters_info<-paste0("You have" ,code(raster::nlayers(data$Env)),"layers.The extent is xmin=",code(raster::extent(data$Env)@xmin),",xmax=",code(raster::extent(data$Env)@xmax),",ymin=",code(raster::extent(data$Env)@ymin),",ymax=",code(raster::extent(data$Env)@ymax))
       out<-list(out,
-                sidebarPanel(
+                sidebarPanel(width = 3,
                   p('Load environmental rasters for model building or model forecasting'),
                   uiOutput('Envbug'),
                   shinyFilesButton('envfiles', 'Raster selection', 'Please select rasters', FALSE, multiple = TRUE),
@@ -649,13 +696,98 @@ observeEvent(input$file_type,{
                                                  
                                                  
                 ),
-                id = "tabs")
+                id = "tabs"),
+                sidebarPanel(width = 3, h4("Change aesthetics"), 
+                             tabsetPanel(tabPanel("Text", checkboxInput(inputId = "label_axes", 
+                                                                        label = strong("Change labels axes"), 
+                                                                        value = FALSE), conditionalPanel(condition = "input.label_axes == true", 
+                                                                                                         textInput("lab_x", "X-axis:", value = "label x-axis")), 
+                                                  conditionalPanel(condition = "input.label_axes == true", 
+                                                                   textInput("lab_y", "Y-axis:", 
+                                                                             value = "label y-axis")), checkboxInput(inputId = "add_title", 
+                                                                                                                     label = strong("Add title"), value = FALSE), 
+                                                  conditionalPanel(condition = "input.add_title == true", 
+                                                                   textInput("title", "Title:", 
+                                                                             value = "Title")), checkboxInput(inputId = "adj_fnt_sz", 
+                                                                                                              label = strong("Change font size"), 
+                                                                                                              value = FALSE), conditionalPanel(condition = "input.adj_fnt_sz == true", 
+                                                                                                                                               numericInput("fnt_sz_ttl", "Size axis titles:", 
+                                                                                                                                                            value = 12), numericInput("fnt_sz_ax", 
+                                                                                                                                                                                      "Size axis labels:", value = 10)), 
+                                                  checkboxInput(inputId = "rot_txt", label = strong("Rotate text x-axis"), 
+                                                                value = FALSE), checkboxInput(inputId = "adj_fnt", 
+                                                                                              label = strong("Change font"), value = FALSE), 
+                                                  conditionalPanel(condition = "input.adj_fnt == true", 
+                                                                   selectInput("font", "Font", choices = c("Courier", 
+                                                                                                           "Helvetica", "Times"), selected = "Helvetica"))), 
+                                         tabPanel("Theme", conditionalPanel(condition = "input.group != '.'", 
+                                                                            checkboxInput(inputId = "adj_col", 
+                                                                                          label = strong("Change colours"), 
+                                                                                          value = FALSE), conditionalPanel(condition = "input.adj_col", 
+                                                                                                                           selectInput(inputId = "palet", label = strong("Select palette"), 
+                                                                                                                                       choices = list(Qualitative = c("Accent", 
+                                                                                                                                                                      "Dark2", "Paired", "Pastel1", 
+                                                                                                                                                                      "Pastel2", "Set1", "Set2", 
+                                                                                                                                                                      "Set3"), Diverging = c("BrBG", 
+                                                                                                                                                                                             "PiYG", "PRGn", "PuOr", 
+                                                                                                                                                                                             "RdBu", "RdGy", "RdYlBu", 
+                                                                                                                                                                                             "RdYlGn", "Spectral"), 
+                                                                                                                                                      Sequential = c("Blues", "BuGn", 
+                                                                                                                                                                     "BuPu", "GnBu", "Greens", 
+                                                                                                                                                                     "Greys", "Oranges", "OrRd", 
+                                                                                                                                                                     "PuBu", "PuBuGn", "PuRd", 
+                                                                                                                                                                     "Purples", "RdPu", "Reds", 
+                                                                                                                                                                     "YlGn", "YlGnBu", "YlOrBr", 
+                                                                                                                                                                     "YlOrRd")), selected = "set1"))), 
+                                                  conditionalPanel(condition = "input.jitter", 
+                                                                   checkboxInput("adj_jitter", strong("Change look jitter"), 
+                                                                                 FALSE), conditionalPanel(condition = "input.adj_jitter", 
+                                                                                                          textInput("col_jitter", "Colour (name or RGB):", 
+                                                                                                                    value = "black"), numericInput("size_jitter", 
+                                                                                                                                                   "Size:", value = 1), sliderInput("opac_jitter", 
+                                                                                                                                                                                    "Opacity:", min = 0, max = 1, 
+                                                                                                                                                                                    value = 0.5, step = 0.01), sliderInput("width_jitter", 
+                                                                                                                                                                                                                           "Width jitter:", min = 0, max = 0.5, 
+                                                                                                                                                                                                                           value = 0.25, step = 0.01))), checkboxInput("adj_grd", 
+                                                                                                                                                                                                                                                                       strong("Remove gridlines"), FALSE), 
+                                                  conditionalPanel(condition = "input.adj_grd", 
+                                                                   checkboxInput("grd_maj", strong("Remove major gridlines"), 
+                                                                                 FALSE), checkboxInput("grd_min", 
+                                                                                                       strong("Remove minor gridlines"), 
+                                                                                                       FALSE)), selectInput("theme", "Theme", 
+                                                                                                                            choices = c(bw = "theme_bw()", classic = "theme_classic()", 
+                                                                                                                                        dark = "theme_dark()", grey = "theme_grey()", 
+                                                                                                                                        light = "theme_light()", line_draw = "theme_linedraw()", 
+                                                                                                                                        minimal = "theme_minimal()"), selected = "theme_bw()")), 
+                                         tabPanel("Legend", conditionalPanel(condition = "input.group != '.'", 
+                                                                             radioButtons(inputId = "adj_leg", label = NULL, 
+                                                                                          choices = c("Keep legend as it is", 
+                                                                                                      "Remove legend", "Change legend"), 
+                                                                                          selected = "Keep legend as it is"), 
+                                                                             conditionalPanel(condition = "input.adj_leg=='Change legend'", 
+                                                                                              textInput("leg_ttl", "Title legend:", 
+                                                                                                        value = "title legend"), selectInput("pos_leg", 
+                                                                                                                                             "Position legend", choices = c("right", 
+                                                                                                                                                                            "left", "top", "bottom"))))), 
+                                         tabPanel("Size", checkboxInput("fig_size", 
+                                                                        strong("Adjust plot size on screen"), 
+                                                                        FALSE), conditionalPanel(condition = "input.fig_size", 
+                                                                                                 numericInput("fig_height", "Plot height (# pixels): ", 
+                                                                                                              value = 480), numericInput("fig_width", 
+                                                                                                                                         "Plot width (# pixels):", value = 480)), 
+                                                  checkboxInput("fig_size_download", 
+                                                                strong("Adjust plot size for download"), 
+                                                                FALSE), conditionalPanel(condition = "input.fig_size_download", 
+                                                                                         numericInput("fig_height_download", 
+                                                                                                      "Plot height (in cm):", value = 14), 
+                                                                                         numericInput("fig_width_download", 
+                                                                                                      "Plot width (in cm):", value = 14)))))
       )
       out
       #####################################################
       out<-list(out,
                 column(12,
-                       sidebarPanel(
+                       sidebarPanel(width = 3,
                          p('Occurrence table'),
                          selectInput("file_type","Type of file:", list(`text (csv)` = "text", 
                                                                        Excel = "Excel", SPSS = "SPSS", 
@@ -678,12 +810,12 @@ observeEvent(input$file_type,{
                          myActionButton("load2",label=("Load"), "primary")
                          #actionButton('load2', 'Select studied specie')
                        ),
-                       mainPanel(width = 6, tabsetPanel(type = "tabs",
+                       mainPanel(width = 7, tabsetPanel(type = "tabs",
                                                         tabPanel("Preview",
                                                                  uiOutput('Occbug'),
                                                                  dataTableOutput('occ')))
                                  ,
-                                 id = "tabs2"))
+                                 id = "tabs"))
       )
     }
     )
